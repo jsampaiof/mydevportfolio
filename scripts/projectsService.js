d994b7bcecfd286b82ec49c00bf4cd4a5d61c37a
Josther Sampaio <jostherfontes@hotmail.com>
2023-03-21 23:36:37 -0300

    Style: start styling the project page

--- scripts/projectsService.js
+++ scripts/projectsService.js
@@ -2,10 +2,51 @@ const projects = [
     {
         id: 1,
         name: 'My Portifolio',
-        desc: 'aaaaa',
+        desc: 'Portifolio desenvolvido com o intuito de apresentar todos os meus projetos, desenvolvidos ou em desenvolvimento, além de servir como uma forma de testar meus conhecimentos nas tecnologias aprendidas. O My Portfolio foi inteiramente desenvolvido por mim, desde a primeira div até suas rotas, definidas com o auxilio do express. As tecnologias utilizadas estão listadas abaixo.',
         photo:{
             src: 'images/02.webp',
             alt: 'Project thumbnail'
+        },
+        techs: {
+            html:{
+                name:'HTML',
+                src:'/images/technologies/HTML 5.webp',
+                alt:'HTML 5'
+            },
+            css:{
+                name:'CSS',
+                src: '/images/technologies/CSS 3.webp',
+                alt: 'CSS 3'
+            },
+            javascript:{
+                name:'javascript',
+                src: '/images/technologies/javascript.webp',
+                alt: 'JavaScript'
+            },
+            git:{
+                name:'git',
+                src: '/images/technologies/git.webp',
+                alt: 'Git'
+            },
+            node:{
+                name:'nodejs',
+                src: '/images/technologies/nodejs.webp',
+                alt: 'Node.JS'
+            },
+            express:{
+                name:'express',
+                src: '/images/technologies/express-js.webp',
+                alt: 'Express'
+            },
+            ejs:{
+                name:'ejs',
+                src: '/images/technologies/ejs.webp',
+                alt: 'EJS'
+            }
+        },
+        updates:{
+            1: 'aaaaaa',
+            2: 'bbbbbb'
         }
     },
     {
@@ -15,7 +56,8 @@ const projects = [
         photo:{
             src: 'images/02.webp',
             alt: 'Project thumbnail'
-        }
+        },
+        updates:{}
     },
     {
         id: 3,
@@ -24,7 +66,8 @@ const projects = [
         photo:{
             src: 'images/02.webp',
             alt: 'Project thumbnail'
-        }
+        },
+        updates:{}
     }
 ];
 
